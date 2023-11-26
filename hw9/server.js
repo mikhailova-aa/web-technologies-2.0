1ccc19a040d4418f80233f2cc6308d2d3fdc4999
mikhailova-aa <92926012+mikhailova-aa@users.noreply.github.com>
2023-11-26 14:17:11 +0300

    Add files via upload

--- hw9/server.js
+++ hw9/server.js
@@ -1,83 +1,57 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
-const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidv4()}`);
-});
-
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room });
-});
-
-const rooms = new Map();
-
-const MESSAGE_TYPES = {
-  JOIN_ROOM: 'join-room',
-  USER_CONNECTED: 'user-connected',
-  OFFER: 'offer',
-  ANSWER: 'answer',
-  ICE_CANDIDATE: 'ice-candidate',
-  JOINED_ROOM: 'joined-room',
-};
+let clients = [];
 
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
-    const data = JSON.parse(message);
-    const { type, roomId, userId, targetUserId, offer, answer, candidate } = data;
+    const parsedMessage = JSON.parse(message);
 
-    if (type === MESSAGE_TYPES.JOIN_ROOM) {
-      ws.roomId = roomId;
-      if (!rooms.has(roomId)) {
-        rooms.set(roomId, []);
+    if (parsedMessage.type === 'offer') {
+      // Принимаем предложение и создаем ответ
+      const otherClient = clients.find(client => client !== ws);
+      if (otherClient) {
+        otherClient.send(JSON.stringify({ type: 'offer', offer: parsedMessage.offer, userId: parsedMessage.userId }));
       }
-      rooms.get(roomId).push(ws);
-
-      // Если у клиента нет userId, присвоить новый уникальный userId
-      ws.userId = userId || uuidv4();
-
-      rooms.get(roomId).forEach((client) => {
-        if (client !== ws && client.readyState === WebSocket.OPEN) {
-          client.send(JSON.stringify({ type: MESSAGE_TYPES.USER_CONNECTED, userId: ws.userId }));
-        }
-      });
-
-      ws.send(JSON.stringify({ type: MESSAGE_TYPES.joined_room, roomId, userId: ws.userId }));
-
-      ws.on('close', () => {
-        if (rooms.has(roomId)) {
-          rooms.get(roomId).forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify({ type, userId: ws.userId, roomId, [type]: type === MESSAGE_TYPES.OFFER ? offer : answer }));
-            }
-          });
-        }
-      });
-    } else if (type === MESSAGE_TYPES.OFFER || type === MESSAGE_TYPES.ANSWER) {
-      if (rooms.has(roomId)) {
-        rooms.get(roomId).forEach((client) => {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ type, userId: ws.userId, [type]: type === MESSAGE_TYPES.OFFER ? offer : answer }));
-          }
-        });
+    } else if (parsedMessage.type === 'answer') {
+      // Пересылаем ответ другому клиенту
+      const otherClient = clients.find(client => client !== ws);
+      if (otherClient) {
+        otherClient.send(JSON.stringify({ type: 'answer', answer: parsedMessage.answer, userId: parsedMessage.userId }));
       }
-    } else if (type === MESSAGE_TYPES.ICE_CANDIDATE) {
-      if (rooms.has(roomId)) {
-        rooms.get(roomId).forEach((client) => {
-          if (client !== ws && client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({ type: MESSAGE_TYPES.ICE_CANDIDATE, userId: ws.userId, candidate }));
-          }
-        });
+    } else if (parsedMessage.type === 'iceCandidate') {
+      // Пересылаем кандидата другому клиенту
+      const otherClient = clients.find(client => client !== ws);
+      if (otherClient) {
+        otherClient.send(JSON.stringify({ type: 'iceCandidate', iceCandidate: parsedMessage.iceCandidate, userId: parsedMessage.userId }));
       }
-    } 
+    } else if (parsedMessage.type === 'startVideoChat') {
+      // Здесь вы можете добавить логику для начала видеочата, если необходимо
+      console.log(`Received startVideoChat request from ${parsedMessage.userId} to ${parsedMessage.partnerUserId}`);
+      const partner = clients.find(client => client.userId === parsedMessage.partnerUserId);
+      if (partner) {
+        // Отправляем запрос на начало видеочата партнеру
+        partner.send(JSON.stringify({ type: 'startVideoChat', partnerUserId: parsedMessage.userId }));
+      } else {
+        console.error(`Partner not found for ${parsedMessage.partnerUserId}`);
+      }
+    } else if (parsedMessage.type === 'userId') {
+      // Сохраняем userId в объекте клиента
+      ws.userId = parsedMessage.userId;
+      clients.push(ws);
+    }
+  });
+
+  ws.on('close', () => {
+    // Удаляем закрытое соединение из списка клиентов
+    clients = clients.filter(client => client !== ws);
   });
 });
 
